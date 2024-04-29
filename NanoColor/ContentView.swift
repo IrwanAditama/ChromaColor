d96d7cd1d744b29ae6dba8abb88f834b8aa3e51c
Irwan Aditama <irwanadaapple@gmail.com>
2024-04-29 10:09:21 +0700

    Initial Commit

---
+++ NanoColor/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  NanoColor
+//
+//  Created by aditamairwan on 24/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            ColorExtractor()
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NanoColor/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  NanoColor
+//
+//  Created by aditamairwan on 24/04/24.
+//
+
+import Foundation
